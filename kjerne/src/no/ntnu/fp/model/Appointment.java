5fa7766d79085eca51e090a286779a9a968cb657
Hilde Njoten <hildeknn@gmail.com>
2012-03-20 15:29:26 +0100

    jobbet litt i appointment

--- kjerne/src/no/ntnu/fp/model/Appointment.java
+++ kjerne/src/no/ntnu/fp/model/Appointment.java
@@ -1,5 +1,50 @@
 package no.ntnu.fp.model;
 
+import java.util.Date;
+/**
+ * 
+ * @author Hilde Kristin
+ * 
+ */
 public class Appointment {
-
-}
+	
+	private Date startTime;
+	private Date endTime;
+	private String description;
+	private Meetingroom meetingRoom;
+	/**
+	 * 
+	 * @param startTime
+	 */
+	public void setStartTime(Date startTime){
+		this.startTime = startTime;
+	}
+	
+	public void setEndTime(Date endTime){
+		this.endTime = endTime;
+	}
+	
+	public void setDescription(String description){
+		this.description = description;
+	}
+	
+	public void setLocation(Meetingroom meetingRoom){
+		this.meetingRoom = meetingRoom;
+	}
+	
+	public Date getStart(){
+		return startTime;
+	}
+	
+	public Date getEnd(){
+		return endTime;
+	}
+	
+	public String getDescription(){
+		return description;
+	}
+	
+	public Meetingroom getLocation(){
+		return meetingRoom;
+	}
+	}
