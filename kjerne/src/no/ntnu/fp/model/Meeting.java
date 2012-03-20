9ea90b48a5354d6ab8c8ee4e20017610fc59787d
Oyvind Nergard <oyvinne@stud.ntnu.no>
2012-03-20 14:19:17 +0100

    Har gjort litt mer paa modellen

--- kjerne/src/no/ntnu/fp/model/Meeting.java
+++ kjerne/src/no/ntnu/fp/model/Meeting.java
@@ -1,5 +1,112 @@
 package no.ntnu.fp.model;
 
-public class Meeting {
+import java.util.ArrayList;
+import java.util.Date;
 
+/**
+ * 
+ * @author Øyvind
+ * 
+ */
+public class Meeting {
+	/**
+	 * Person møteleder
+	 */
+	private Person meetingLeader;
+	
+	/**
+	 * Start
+	 */
+	private Date startTime;
+	
+	/**
+	 * Slutt
+	 */
+	private Date endTime;
+	
+	/**
+	 * Beskrivelse av møtet
+	 */
+	private String description;
+	
+	/**
+	 * Møterom 
+	 */
+	private Meetingroom meetingRoom;
+	
+	/**
+	 * Deltakere
+	 */
+	private ArrayList<Person> participants = new ArrayList<Person>();
+	
+	/**
+	 * Innkallelser
+	 */
+	private Meetingrequest meetingRequests;
+	
+	/**
+	 * Konstruktør - Legg inn det som trengs
+	 */
+	public Meeting() {
+		
+	}
+	
+	public Person getMeetingLeader() {
+		return meetingLeader;
+	}
+
+	public void setMeetingLeader(Person meetingLeader) {
+		this.meetingLeader = meetingLeader;
+	}
+
+	public Date getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+
+	public Date getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Meetingroom getMeetingRoom() {
+		return meetingRoom;
+	}
+
+	public void setMeetingRoom(Meetingroom meetingRoom) {
+		this.meetingRoom = meetingRoom;
+	}
+
+	public ArrayList<Person> getParticipants() {
+		return participants;
+	}
+	
+	/**
+	 * Må legge til i participants, samt å sende innkallelse
+	 */
+	public void addParticipant(Person participant) {
+		
+	}
+	
+	/**
+	 * Fjerner fra participants, samt fjerner innkallelse
+	 */
+	public void removeParticipant(Person participant){
+		
+	}
+	
 }
--- kjerne/src/no/ntnu/fp/model/Meetingrequest.java
+++ kjerne/src/no/ntnu/fp/model/Meetingrequest.java
@@ -1,5 +1,27 @@
 package no.ntnu.fp.model;
 
+/**
+ * 
+ * @author Øyvind
+ * 
+ */
 public class Meetingrequest {
-
+	/**
+	 * Det aktuelle møtet 
+	 */
+	private Meeting meeting;
+	
+	/**
+	 * Den aktuelle personen
+	 */
+	private Person participant;
+	
+	/**
+	 * True om deltakeren er med
+	 */
+	private boolean attending;
+	
+	/**
+	 * Konstruktør
+	 */
 }
