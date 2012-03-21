bdbe422007f3d0bff11db00643ce6b57053b9ea4
Hilde Njoten <hildeknn@gmail.com>
2012-03-21 12:12:07 +0100

    jeg har jobbet med appointment

--- kjerne/src/no/ntnu/fp/model/Appointment.java
+++ kjerne/src/no/ntnu/fp/model/Appointment.java
@@ -12,10 +12,25 @@ public class Appointment {
 	private Date endTime;
 	private String description;
 	private Meetingroom meetingRoom;
+	
 	/**
 	 * 
 	 * @param startTime
 	 */
+	
+	public Appointment(Date startTime, Date endTime, String description){
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.description = description;	
+	}
+	
+	public Appointment(Date startTime, Date endTime, String description, Meetingroom meetingRoom){
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.description = description;	
+		this.meetingRoom = meetingRoom;
+	}
+	
 	public void setStartTime(Date startTime){
 		this.startTime = startTime;
 	}
